de1e02ba9304bf299af967671eb2f8b624964f9c
AfroMonkey <moisalejandro@gmail.com>
2016-08-16 17:36:09 -0500

    [IMP] Add update_variants_code

--- models/product_template.py
+++ models/product_template.py
@@ -1,7 +1,25 @@
-from openerp import api, fields, models
+from openerp import _, api, fields, models, exceptions
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     default_variants_code = fields.Char()
+
+    @api.one
+    def update_variants_code(self):
+        if not self.default_variants_code:
+            raise exceptions.ValidationError("The code can not be empty")
+        else:
+            color = ''
+            size = ''
+            for product in self.product_variant_ids:
+                for attribute in product.attribute_value_ids:
+                    if attribute.attribute_id.name == 'Talla':
+                        size = attribute.name
+                        break
+                for attribute in product.attribute_value_ids:
+                    if attribute.attribute_id.name == 'Color':
+                        color = attribute.name
+                        break
+                product.default_code = self.default_variants_code + '-' + color + '-' + size
